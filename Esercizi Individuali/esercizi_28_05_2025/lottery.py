b79a7773a9407a12d49c7d2db077bcc152939a6e
FullestSdg <sdgsergiodg20@gmail.com>
2025-05-29 23:59:49 +0200

    updated

--- Esercizi Individuali/esercizi_28_05_2025/lottery.py
+++ Esercizi Individuali/esercizi_28_05_2025/lottery.py
@@ -2,28 +2,34 @@
 9-14. Lottery: Create a class LotteryMachine that holds a list containing a series of 10 numbers and 5 letters. Implement a method to randomly select 4 items (numbers or letters) from this list to draw a winning ticket. Finally, implement a method to display a message saying that any ticket matching the winning 4 items wins a prize.
 
 '''
-import random
-
-from typing import Any
-
+from random import choice
 
 class LotteryMachine:
 
-    def __init__(self, vincita:list[Any]=[1,2,3,4,5,6,7,8,9,0,"h","c","a","o"]) -> None:
+    def __init__(self) -> None:
+        
+        self.pool: list[str] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'a', 'b', 'c', 'd', 'e']
+        self.winning_ticket: list[str] = [] 
 
-        self.vincita = vincita
+    def draw_winning_ticket(self) -> None:
+        
+        while len(self.winning_ticket) < 4:
 
-    def random_selection(self, biglietto_vincente:str = None) -> None:
+            pulled: str = choice(self.pool)
 
-        self.biglietto_vincente = random.randint(4,self.vincita)
+            if pulled not in self.winning_ticket:
+                self.winning_ticket.append(pulled)
 
-    def winning_message(self) -> str:
+    def announce_winner(self) -> None:
 
-        return f"Se il vostro biglietto è {self.biglietto_vincente}!\nCongratulazioni avete vinto!!!! "
+        print("The winning ticket is:", self.winning_ticket)
+        print("Any ticket matching these 4 items wins a prize!")
 
 
-lottery:LotteryMachine = LotteryMachine()
-print(lottery.random_selection())
+
+lottery = LotteryMachine()
+lottery.draw_winning_ticket() 
+lottery.announce_winner()  
 
 
 
