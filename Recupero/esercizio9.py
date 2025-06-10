6a314ff714d5db662e906e14c171d384e61858ae
FullestSdg <sdgsergiodg20@gmail.com>
2025-06-10 12:38:13 +0200

    updated

--- Recupero/esercizio9.py
+++ Recupero/esercizio9.py
@@ -1,6 +1,28 @@
-x = "ciao"
-key = 2
+# CIFRARIO DI CESARE
 
-l:str = "abcdefghilmnopqrstuvz"
+from string import ascii_lowercase
 
+frase:str = "flavio"
+
+def cifrario(frase:str, chiave:int) -> str:
+
+    # Stringa che conterrà il risultato
+
+    risultato:str = ""
+
+    # Voglio trovare l'indice corrispondente al carattere corrente Esempio carattere="a" idx = 0
+    # perchè ascii_lowercase = ["a", "b", "c", ...]
+
+    for carattere in frase:
+
+        idx:int = ascii_lowercase.index(carattere)
+
+        idx = (idx + chiave) % len(ascii_lowercase)
+
+        risultato += ascii_lowercase[idx]
+
+    return risultato
+
+print(cifrario(frase, 2))
+                                     
 
