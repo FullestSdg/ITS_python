5194983ca2189b52849d9b48d905781e43237e43
FullestSdg <sdgsergiodg20@gmail.com>
2025-07-18 19:14:01 +0200

    updated

--- Recupero/Recupero 1/esercizio2.py
+++ Recupero/Recupero 1/esercizio2.py
@@ -19,3 +19,71 @@ Il numero 3 compare 5 volte nella sequenza (attenzione all'output se il numero c
 Il numero 3 compare per la prima volta in posizione 3 nella sequenza
 La somma dei valori della sequenza diversi da 3 e' 26
 '''
+
+x:int = int(input("Inserisci un numero: "))
+
+if x < 0:
+
+    raise ValueError("Numero inserito non valido, deve essere maggiore di 0")
+
+def checkSequenza(lista_sequenza:list[int]) -> str:
+
+    contatore_frequenza_x = 0
+
+    for numbers in lista_sequenza:
+        
+        if numbers == x:
+
+            contatore_frequenza_x += 1 
+
+        else:
+            continue
+    
+    pos_x = 0
+
+    for numbers in lista_sequenza:
+
+        if contatore_frequenza_x == 1:
+
+            if numbers == x:
+
+                pos_x += lista_sequenza.index(x)
+
+        else:
+
+            if numbers == x:
+
+                new_pos_x = lista_sequenza.index(x)
+
+                pos_x = new_pos_x
+
+    somma_valori_diversi_x = 0
+
+    for numbers in lista_sequenza:
+
+        if numbers != x:
+
+            somma_valori_diversi_x += numbers 
+        
+        else:
+            continue
+    
+    return f"La sequenza è {lista_sequenza}.\nIl numero {x} compare {contatore_frequenza_x} volte nella sequenza.\nIl numero {x} compare per la prima volta in posizione {pos_x} della sequenza.\nLa somma dei valori della sequenza diversi da {x} è {somma_valori_diversi_x}"
+
+lista_sequenza = []
+
+while True:
+
+    numero_della_sequenza:int = int(input("Inserisci una sequenza di numeri, digitare 0 per fermare: "))
+
+    if numero_della_sequenza == 0:
+        print(checkSequenza(lista_sequenza))
+        break
+
+    elif numero_della_sequenza < 0:
+        raise ValueError("Errore il numero inserito non è positivo")
+    
+    else:
+        lista_sequenza.append(numero_della_sequenza)
+
+
